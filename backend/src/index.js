25c27db19c7d6599f99b1b0fcb67227269c6366a
Aditya Tile <adityatile99@gmail.com>
2025-01-08 23:34:29 +0530

    add controllers and routes folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
-import express from 'express';
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
 const port = 3000;
 
 const app = express();
+
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
