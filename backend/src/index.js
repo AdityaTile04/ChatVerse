bc819f94d40115c41e474ee45ba6e5f618bc5249
Aditya Tile <adityatile99@gmail.com>
2025-01-11 10:39:30 +0530

    Implement signup functionality with JWT token generation and validation

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,10 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 const app = express();
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-  connectDB()
+  connectDB();
 });
