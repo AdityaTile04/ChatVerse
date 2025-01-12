8bbb115252f3b0d8cbd18e804fd28392dbcc1e6f
Aditya Tile <adityatile99@gmail.com>
2025-01-12 21:15:07 +0530

    create message route and controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 const port = process.env.PORT;
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
