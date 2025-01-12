f454d0e16e2ca82197bfd378aea448ff1d7d9aeb
Aditya Tile <adityatile99@gmail.com>
2025-01-12 22:29:17 +0530

    implement get messages function for sending and receiving messages

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -10,6 +11,24 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(200).json(filteredUsers);
   } catch (error) {
     console.log("Error in getUserForSidebar");
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receivedId: userToChatId },
+        { senderId: userToChatId, receivedId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages conteroller", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
