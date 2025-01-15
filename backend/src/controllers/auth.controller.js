060247604ad668775064b1b064e5e118faf35e21
Aditya Tile <adityatile99@gmail.com>
2025-01-15 22:53:18 +0530

    frontend: create profile page and add image upload functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -91,25 +91,23 @@ export const logout = (req, res) => {
 
 export const updateProfile = async (req, res) => {
   try {
-    const { profilePicture } = req.body;
+    const { profilePic } = req.body;
     const userId = req.user._id;
 
-    if (!profilePicture) {
-      res.status(400).json({ message: "Profile pic is required" });
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
     }
 
-    const uploadResponse = await cloudinary.uploader.upload(profilePicture);
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
     const updatedUser = await User.findByIdAndUpdate(
       userId,
-      {
-        profilePicture: uploadResponse.secure_url,
-      },
+      { profilePic: uploadResponse.secure_url },
       { new: true }
     );
 
-    res.status(200).json({ updatedUser });
+    res.status(200).json(updatedUser);
   } catch (error) {
-    console.log("error in update profile");
+    console.log("error in update profile:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
